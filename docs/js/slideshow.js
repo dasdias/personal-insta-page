355fec7756ad4e44465d8e4bac051a3984823076
Alex Fix <dasdias@yandex.ru>
2018-11-02 22:09:35 +0300

    Sleder
    Добавлен градиентный фон, слайдер, скрипт слайдера.

---
+++ docs/js/slideshow.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	var slideIndex = 0;
+
+	showPhotos();
+
+	function showPhotos(){
+		var slides = $('.photo img');
+		console.log(slides);
+		console.log(slides.length);
+
+		for (var i = 0; i< slides.length; i++) {
+			console.log(i);
+			$(slides[i]).hide();
+		}
+
+		slideIndex++;
+
+		if (slideIndex > slides.length) {
+			slideIndex = 1;
+		}
+		console.log(slideIndex);
+
+		$(slides[slideIndex-1]).show();
+
+		setTimeout(showPhotos,1000);
+	}
+	
+});
